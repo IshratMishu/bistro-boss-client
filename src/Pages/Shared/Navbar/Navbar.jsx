f1094beebcb490cf3d73ca09f59932796a3ba477
IshratMishu <ishratjahan0425@gmail.com>
2024-10-28 18:49:59 +0600

    login before add to cart

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,50 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Navbar = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error))
+    }
 
     const navOptions = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/menu'>Our Menu</Link></li>
         <li><Link to='/order/salad'>Order Food</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-       
+        <li><Link to='/'>  
+            <div className="indicator">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+            <span className="badge badge-sm indicator-item">0</span>
+            </div>
+        </Link></li>
+
+
+        {user ?
+            <>
+                <span>{user?.displayName}</span>
+                <button onClick={handleLogout} className="btn btn-ghost">LogOut</button>
+            </> :
+            <>
+                <li><Link to='/login'>Login</Link></li>
+            </>
+        }
+
     </>
 
     return (
@@ -36,8 +73,8 @@ const Navbar = () => {
                 </div>
                 <a className="btn btn-ghost text-xl">
                     <div className="flex flex-col text-center leading-none text-lg">
-                    <span className="text-3xl">BISTRO BOSS</span>
-                    <span>R E S T A U R E N T</span>
+                        <span className="text-3xl">BISTRO BOSS</span>
+                        <span>R E S T A U R E N T</span>
                     </div>
                 </a>
             </div>
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,34 @@
+import Swal from "sweetalert2";
+import useAuth from "../../Hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
     const { name, recipe, price, image } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+
+    const handleAddToCart = food => {
+        if (user && user.email) {
+//y
+        } else {
+            Swal.fire({
+                title: "You are not logged in yet!",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Login here"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                        navigate('/login');
+                }
+            });
+        }
+    }
+
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
             <figure>
@@ -14,7 +41,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 mx-auto bg-slate-100 border-orange-400">Add to Cart</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mx-auto bg-slate-100 border-orange-400">Add to Cart</button>
                 </div>
             </div>
         </div>
